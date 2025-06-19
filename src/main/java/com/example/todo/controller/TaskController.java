948681d8e251f6e0699932a6ffa687d78a328a13
EwelinaPaczkowska <s30995@pjwstk.edu.pl>
2025-06-19 14:41:10 +0200

    Added TaskController

---
+++ src/main/java/com/example/todo/controller/TaskController.java
@@ -0,0 +1,25 @@
+package com.example.todo.controller;
+
+import com.example.todo.model.Task;
+import com.example.todo.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.findAllTasks();
+    }
+}
