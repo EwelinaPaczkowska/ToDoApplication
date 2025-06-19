5882c22bd82361c4259471cc77e20bf34c3b5b68
EwelinaPaczkowska <s30995@pjwstk.edu.pl>
2025-06-19 14:31:44 +0200

    Added TaskService

---
+++ src/main/java/com/example/todo/service/TaskService.java
@@ -0,0 +1,22 @@
+package com.example.todo.service;
+
+import com.example.todo.model.Task;
+import com.example.todo.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class TaskService {
+    @Autowired
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> findAllTasks(){
+        return taskRepository.findAll();
+    }
+}
